4b2799b42d92604b41d01c78135fc6fbea91f684
Ray Lam <ray.lam@oocl.com>
2020-12-09 23:37:54 +0800

    [React-TodoList][Ray Lam] feat: add react-select

--- src/Components/TodoItem/index.jsx
+++ src/Components/TodoItem/index.jsx
@@ -4,9 +4,28 @@ import { deleteToDo, updateTodo } from '../../apis/todo';
 import '../common.css';
 import { Row, Col } from 'antd';
 import { Button, Radio } from 'antd';
+import { Tag, Divider } from 'antd';
+import Select from 'react-select'
 
+const options = [
+    { value: 'sports', label: 'Sports' },
+    { value: 'shopping', label: 'Shopping' },
+    { value: 'movie', label: 'Movie' },
+    { value: 'reading', label: 'Reading' },
+    { value: 'music', label: 'Music' },
+    { value: 'training', label: 'Training' },
+    { value: 'work', label: 'Work' }
+  ];
 
 export default class TodoItem extends Component {
+    state = {
+        selectedOption: '',
+      };
+
+    handleChange = selectedOption => {
+        this.setState({ selectedOption: selectedOption });
+        console.log(selectedOption);
+      };
 
     onToggleDone = () => {
         const { id, text, done } = this.props.item
@@ -20,22 +39,32 @@ export default class TodoItem extends Component {
         deleteToDo(item.id).then((response) => {
             this.props.deleteItem(response.data.id);
         })
-
     }
 
     render() {
         const { text, done } = this.props.item;
         const textSyle = done ? "textStyle" : "";
+       
         return (
             <div className="item">
                 <Row>
-                    <Col span={12}><label className={textSyle} onClick={this.onToggleDone}>{text}</label>-8</Col>
-                    <Col span={4} offset={8}>
-                        <Button type="primary" shape="circle" onClick={this.deleteItem}>X</Button>
+                    <Col span={12}><label className={textSyle} onClick={this.onToggleDone}>{text}</label></Col>
+                    <Col span={4} offset={8}> 
+                    <Button type="primary" shape="circle" onClick={this.deleteItem} danger>X</Button>
                     </Col>
                 </Row>
+                <Row>
+                <div className="selector">
+                    <Select
+                    closeMenuOnSelect={false}
+                    defaultValue={[options[0], options[1]]}
+                    onChange={this.handleChange}
+                    isMulti
+                    options={options}
+                     />
+                </div>
+                </Row>
             </div>
-
         )
     }
 }
